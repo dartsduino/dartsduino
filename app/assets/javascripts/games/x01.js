8314e8f69332db373e2e79010120772033013deb
Ikuo Terado <eqobar@gmail.com>
2014-02-17 23:56:43 +0900

    Add focus in score

--- app/assets/javascripts/games/x01.js
+++ app/assets/javascripts/games/x01.js
@@ -3,6 +3,8 @@
 var Games_x01 = function () {
   console.log('x01: start');
 
+  var focusClass = 'focus';
+
   var dartsUi = window.dartsUi;
 
   var scoreIndex = 1;
@@ -10,11 +12,16 @@ var Games_x01 = function () {
   var totalScore = 301;
   var totalScoreElements = $('.scores-total-score td');
 
+  $(scoreElements[scoreIndex]).addClass(focusClass);
+
   dartsUi.onHit(function (cellId, point, ratio) {
     // console.log(cellId + ' : ' + point + ' x ' + ratio + ' = ' + point * ratio);
 
     $(scoreElements[scoreIndex]).text(point * ratio);
+
+    $(scoreElements[scoreIndex]).removeClass(focusClass);
     scoreIndex++;
+    $(scoreElements[scoreIndex]).addClass(focusClass);
 
     totalScore -= point * ratio;
     $(totalScoreElements).text(totalScore);
