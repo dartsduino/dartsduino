6d36e4348d95acfbb6cabbdf9dfc7f83aea96096
Ikuo Terado <eqobar@gmail.com>
2014-03-23 23:36:24 +0900

    Enable debug mode in darts-ui

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,5 +1,8 @@
 $(function () {
   if ($('#darts-ui')[0]) {
-    window.dartsUi = new DartsUi('#darts-ui');
+    var dartsUi = new DartsUi('#darts-ui');
+    dartsUi.setDebugMode(true);
+
+    window.dartsUi = dartsUi;
   }
 });
